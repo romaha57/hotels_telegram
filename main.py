93ce8324bd4f038bac00bcb6fad56f9ff057f3d3
Дмитрий Лоханин <chip1732@mail.ru>
2022-03-13 02:21:10 +0600

    init template

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import start_bot
-
+from loader import bot
+import handlers
+from utils.set_bot_commands import set_default_commands
 
 if __name__ == '__main__':
-    start_bot.get_start_message
+    set_default_commands(bot)
+    bot.infinity_polling()
